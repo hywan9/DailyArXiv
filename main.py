5bc90fce8de55aba65dea2f74b2a581504c21df7
hywan9 <hywan9@gmail.com>
2025-10-29 12:34:53 +0800

    update keywords

--- main.py
+++ main.py
@@ -6,28 +6,26 @@ from datetime import datetime
 from utils import get_daily_papers_by_keyword_with_retries, generate_table, back_up_files, \
     restore_files, remove_backups, get_daily_date
 
-beijing_timezone = pytz.timezone('Asia/Shanghai')
+timezone = pytz.timezone('Asia/Shanghai')
 
-# NOTE: arXiv API seems to sometimes return an unexpected empty list.
+current_date = datetime.now(timezone).strftime("%Y-%m-%d")
 
-# get current beijing time date in the format of "2021-08-01"
-current_date = datetime.now(beijing_timezone).strftime("%Y-%m-%d")
 # get last update date from README.md
 with open("README.md", "r") as f:
     while True:
         line = f.readline()
-        if "Last update:" in line: break
+        if "Last update:" in line:
+            break
     last_update_date = line.split(": ")[1].strip()
     # if last_update_date == current_date:
     # sys.exit("Already updated today!")
 
-keywords = ["Time Series"]  # TODO add more keywords
+keywords = ["Time Series Forecasting"]  # TODO add more keywords
 
 max_result = 100  # maximum query results from arXiv API for each keyword
 issues_result = 15  # maximum papers to be included in the issue
 
 # all columns: Title, Authors, Abstract, Link, Tags, Comment, Date
-# fixed_columns = ["Title", "Link", "Date"]
 
 column_names = ["Title", "Link", "Abstract", "Date", "Comment"]
 
@@ -43,20 +41,23 @@ f_rm.write(
 # write to ISSUE_TEMPLATE.md
 f_is = open(".github/ISSUE_TEMPLATE.md", "w")  # file for ISSUE_TEMPLATE.md
 f_is.write("---\n")
-f_is.write("title: Latest {0} Papers - {1}\n".format(issues_result, get_daily_date()))
+f_is.write(
+    "title: Latest Papers - {0}\n".format(get_daily_date()))
 f_is.write("labels: documentation\n")
 f_is.write("---\n")
 f_is.write(
-    "**Please check the [Github](https://github.com/ke1ewang/DailyArXiv) page for a better reading experience and more papers.**\n\n")
+    "**Please check the [Github](https://github.com/hywan9/DailyArXiv) page for a better reading experience and more papers.**\n\n")
 
 for keyword in keywords:
     f_rm.write("## {0}\n".format(keyword))
     f_is.write("## {0}\n".format(keyword))
     if len(keyword.split()) == 1:
-        link = "AND"  # for keyword with only one word, We search for papers containing this keyword in both the title and abstract.
+        # for keyword with only one word, We search for papers containing this keyword in both the title and abstract.
+        link = "AND"
     else:
         link = "OR"
-    papers = get_daily_papers_by_keyword_with_retries(keyword, column_names, max_result, link)
+    papers = get_daily_papers_by_keyword_with_retries(
+        keyword, column_names, max_result, link)
     if papers is None:  # failed to get papers
         print("Failed to get papers!")
         f_rm.close()
